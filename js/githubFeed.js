10848add23f748c0e8ece7e324ebc6015220e385
Cipher-Coder <matthew.rebehn@gmail.com>
2019-04-11 21:17:08 -0400

    first commit

---
+++ js/githubFeed.js
@@ -0,0 +1,89 @@
+var GithubFeed = {
+  init: function(config) {
+    this.count = config.count || 0;
+    this.order = config.order || "desc";
+    this.url =
+      "https://api.github.com/users/" +
+      config.username +
+      "/repos?per_page=" +
+      this.count +
+      "&sort=created&direction=" +
+      this.order;
+    this.container = config.container;
+    this.onComplete = config.onComplete || function() {};
+    this.fetch();
+  },
+
+  xmlHttp: function() {
+    return new XMLHttpRequest();
+  },
+
+  objJSON: function(options, callback) {
+    var self = this;
+
+    var cached = self.getCache();
+    if (cached !== null) {
+      callback(JSON.parse(cached));
+      return;
+    }
+
+    var xhttp = self.xmlHttp();
+    options.url = options.url || location.href;
+    xhttp.open("GET", options.url, true);
+    xhttp.send(null);
+
+    xhttp.onreadystatechange = function() {
+      if (xhttp.status === 200 && xhttp.readyState === 4) {
+        self.setCache(xhttp.responseText);
+        callback(xhttp.responseText);
+      }
+    };
+  },
+  setCache: function(data) {
+    if (sessionStorage) {
+      sessionStorage.setItem("repos:", JSON.stringify(data));
+    }
+  },
+  getCache: function() {
+    if (sessionStorage) {
+      return sessionStorage.getItem("repos:");
+    } else {
+      return false;
+    }
+  },
+  bindTemplate: function(name, description, url, starCount) {
+    var container = "";
+
+    container += '<div class="repo-container">';
+    container +=
+      '<h3 class="repo-name"><a href="' + url + '">' + name + "</a></h3>";
+    container += '<div class="repo-description">' + description + "</div>";
+    container += "</div>";
+
+    return container;
+  },
+
+  fetch: function() {
+    var self = this;
+
+    self.objJSON({ url: self.url }, function(response) {
+      var repos = JSON.parse(response),
+        reposList = document.querySelector(self.container),
+        content = "",
+        i;
+
+      for (i = 0; i < repos.length; i++) {
+        content += self.bindTemplate(
+          repos[i].name,
+          repos[i].description,
+          repos[i].svn_url,
+          repos[i].stargazers_count
+        );
+      }
+
+      reposList.innerHTML = content;
+
+      self.onComplete();
+    });
+  }
+};
---
+++ js/main.js
@@ -0,0 +1,124 @@
+new GitHubCalendar(".calendar", "Cipher-Coder", { responsive: true });
+
+GithubFeed.init({
+  username: "Cipher-Coder",
+  container: "#github-feeds",
+  count: 8,
+  order: "desc",
+  onComplete: function() {
+    console.log("Feed Loaded");
+  }
+});
+
+// Start of Clock and Calendar
+
+let tday = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+let tmonth = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
+];
+
+function GetClock() {
+  let d = new Date();
+  let nday = d.getDay(),
+    nmonth = d.getMonth(),
+    ndate = d.getDate(),
+    nyear = d.getFullYear();
+  let nhour = d.getHours(),
+    nmin = d.getMinutes(),
+    nsec = d.getSeconds(),
+    ap;
+
+  if (nhour == 0) {
+    ap = " AM";
+    nhour = 12;
+  } else if (nhour < 12) {
+    ap = " PM";
+  } else if (nhour == 12) {
+    ap = " PM";
+  } else if (nhour > 12) {
+    ap = " PM";
+    nhour -= 12;
+  }
+
+  if (nmin <= 9) nmin = "0" + nmin;
+  if (nsec <= 9) nsec = "0" + nsec;
+
+  let clocktext =
+    "" +
+    tday[nday] +
+    ", " +
+    tmonth[nmonth] +
+    " " +
+    ndate +
+    ", " +
+    nyear +
+    "  " +
+    nhour +
+    ":" +
+    nmin +
+    ":" +
+    nsec +
+    ap +
+    "";
+
+  document.getElementById("clock").textContent = clocktext;
+}
+
+GetClock();
+
+setInterval(GetClock, 1000);
+
+// Start of Weather Widget
+
+const apiKey = "de86474ef2fa9963ad1c2292b7b89082"; //Please change to your own API key. They are free.
+let cityEl = document.getElementById("city");
+let currTempEl = document.getElementById("temp");
+let humidityEl = document.getElementById("humidity");
+let windEl = document.getElementById("wind");
+let skyEl = document.getElementById("sky");
+
+let lat = 35.398775; //Location you want your weather for.
+let lon = -84.346465;
+
+function findWeather() {
+  let searchLink =
+    "https://api.openweathermap.org/data/2.5/weather?lat=" +
+    lat +
+    "&lon=" +
+    lon +
+    "&appid=" +
+    apiKey +
+    "&units=imperial"; // Change this to metric or leave as imperial if in the US
+  httpRequestAsync(searchLink, theResponse);
+}
+
+function theResponse(response) {
+  let jsonObject = JSON.parse(response);
+  cityEl.textContent = jsonObject.name; // Location
+  currTempEl.textContent = parseInt(jsonObject.main.temp) + "° "; //Temperature
+  humidityEl.textContent = jsonObject.main.humidity + "%"; // Humidity
+  windEl.textContent = jsonObject.wind.speed + "mph "; // Wind Speed
+  skyEl.textContent = jsonObject.clouds.all + "%"; // Cloud Cover %
+}
+
+function httpRequestAsync(url, callback) {
+  var httpRequest = new XMLHttpRequest();
+  httpRequest.onreadystatechange = () => {
+    if (httpRequest.readyState == 4 && httpRequest.status == 200)
+      callback(httpRequest.responseText);
+  };
+  httpRequest.open("GET", url, true);
+  httpRequest.send();
+}
+findWeather(); //Initiate the function
